631f00c8f0d66c66309d87be0197c6a3900a50ee
Jair Trejo <jair@jairtrejo.mx>
2022-12-07 11:09:55 -0800

    Refactoring day 4 solution

--- day04.py
+++ day04.py
@@ -2,26 +2,24 @@ import fileinput
 import re
 from itertools import tee
 
-gen_pairs = lambda f: (
-    ((a, b), (c, d))
-    for a, b, c, d in (map(int, re.split(r"[,-]", l)) for l in f)
-)
-
-gen_overlaps = lambda pairs: (
-    max(min(b, d) - max(a, c) + 1, 0) for ((a, b), (c, d)) in pairs
-)
-
 with fileinput.input() as f:
-    pairs = gen_pairs(f)
-    overlaps = gen_overlaps(pairs)
-    print(sum(1 for o in overlaps if o > 0))
+    pairs = (
+        ((a, b), (c, d))
+        for a, b, c, d in (map(int, re.split(r"[,-]", l)) for l in f)
+    )
+    overlaps = (
+        (((a, b), (c, d)), max(min(b, d) - max(a, c) + 1, 0))
+        for ((a, b), (c, d)) in pairs
+    )
 
-with fileinput.input() as f:
-    pairs, pairs_prime = tee(gen_pairs(f))
+    overlaps, overlaps_p = tee(overlaps)
+
+    print(sum(1 for _, o in overlaps if o > 0))
+
     print(
         sum(
             1
-            for o, ((a, b), (c, d)) in zip(gen_overlaps(pairs), pairs_prime)
+            for ((a, b), (c, d)), o in overlaps_p
             if o == b - a + 1 or o == d - c + 1
         )
     )
